8a5bc350b7c8242fa3c623d5a51a3074e7faff6c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-21 03:19:18 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,19 +1,5 @@
 class Hash
   def keys_of(arguments)
-   def keys_of(*arguments)
-    array = []
-    self.each do |key, value|
-      arguments.each do |i|
-        if i == value
-        array.push(key)
-        end
-      end 
-
-
-    end
-    return array
-  end
-  
-end
+    # code goes here
   end
 end
